5f5255b9f450c30d5bc34b964844e69bce5e55f4
Konstantin Kukushkin <k.k.o.lukoml@mail.ru>
2025-10-26 15:52:43 +0300

    feat: Find solution for fs methods

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-  // Write your code here
+  const folderPath = path.join(__dirname, 'files');
+  try {
+    const namesFiles =await fs.readdir(folderPath);
+    console.log(namesFiles)
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    }
+  }
 };
 
 await list();
